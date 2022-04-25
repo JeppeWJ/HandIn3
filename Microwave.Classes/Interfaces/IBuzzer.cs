d8c9aacbf5d4ed2e1a0031d8f7479e7e708c8cd5
Anders Eriksen <202008532@post.au.dk>
2022-04-25 11:28:47 +0200

    BuzzerClass

---
+++ Microwave.Classes/Interfaces/IBuzzer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Classes.Interfaces
+{
+    public interface IBuzzer
+    {
+        public void BuzzerOn(string text);
+    }
+}
