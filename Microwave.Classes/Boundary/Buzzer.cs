8f5e7ff79e0e5a036912d479804e6ac193640a13
Anders Eriksen <202008532@post.au.dk>
2022-04-25 11:28:47 +0200

    BuzzerClass

---
+++ Microwave.Classes/Boundary/Buzzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microwave.Classes.Interfaces;
+
+namespace Microwave.Classes
+{
+    class Buzzer: IBuzzer
+    {
+        private IOutput myOutput;
+        public void BuzzerOn (string text)
+        {
+            myOutput.OutputLine(text);
+        }
+    }
+}
